b36b9651da3af50f7473340145c03229409b802d
Thomas Rimbot <thomas.rimbot@mail.polimi.it>
2022-04-06 10:41:50 +0200

    Add first folder

---
+++ Proj_284260_342454_342763/Miniproject_1/model.py
@@ -0,0 +1,23 @@
+import torch
+
+### For mini-project 1
+class Model():
+    def __init__(self) -> None:
+        ## instantiate model + optimizer + loss function + any other stuff you need
+        pass
+
+    def load_pretrained_model(self ) -> None:
+        ## This loads the parameters saved in bestmodel . pth into the model
+        pass
+
+    def train(self, train_input, train_target) -> None:
+        # : train˙input : tensor of size (N , C , H , W ) containing a noisy version of the images.
+        # : train˙target : tensor of size (N , C , H , W ) containing another noisy version of the
+        # same images , which only differs from the input by their noise.
+        pass
+    
+    def predict(self, test_input) -> torch.Tensor:
+        # : test˙input : tensor of size ( N1 , C , H , W ) that has to be denoised by the trained
+        # or the loaded network .
+        # : returns a tensor of the size ( N1 , C , H , W )
+        pass
