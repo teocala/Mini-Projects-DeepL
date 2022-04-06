133dccee28cbd67899f5394b15093c63d1771785
Teocala <matteo.calafa@mail.polimi.it>
2022-04-06 11:16:17 +0200

    Added very basic structure to model

--- Proj_284260_342454_342763/Miniproject_1/model.py
+++ Proj_284260_342454_342763/Miniproject_1/model.py
@@ -1,10 +1,19 @@
-import torch
+import torch 
+from torch import nn
 
 ### For mini-project 1
 class Model():
     def __init__(self) -> None:
         ## instantiate model + optimizer + loss function + any other stuff you need
-        pass
+        super().__init__()
+        
+        self.conv1 = nn.Conv2d(32, 1, kernel_size = 4,  stride = 1, padding = (4 - 1) // 2)
+        self.convT1 = nn.ConvTranspose2d(32, 1, kernel_size=4, stride=1) 
+
+
+        criterion = nn.CrossEntropyLoss()
+        optimizer = torch.optim.SGD(self.parameters(), lr=0.1)
+        
 
     def load_pretrained_model(self ) -> None:
         ## This loads the parameters saved in bestmodel . pth into the model
@@ -14,10 +23,22 @@ class Model():
         # : train˙input : tensor of size (N , C , H , W ) containing a noisy version of the images.
         # : train˙target : tensor of size (N , C , H , W ) containing another noisy version of the
         # same images , which only differs from the input by their noise.
-        pass
+        for epoch in range(100):
+            for batch_input in train_input.split(batch_size=100):
+                output = self.predict(batch_input)
+                self.optimizer.zero_grad()
+                self.criterion.backward()
+                self.optimizer.step()
+                
     
     def predict(self, test_input) -> torch.Tensor:
         # : test˙input : tensor of size ( N1 , C , H , W ) that has to be denoised by the trained
         # or the loaded network .
         # : returns a tensor of the size ( N1 , C , H , W )
-        pass
+        
+        # like the forward method
+        
+        y = self.conv1(test_input)
+        y = self.convT1(y)
+        
+        return y
