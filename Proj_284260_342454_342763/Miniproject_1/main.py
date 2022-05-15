3b6b5963e9dcbfbe219f805f792f0baa20a113b7
Paolo Motta <paolo9.motta@mail.polimi.it>
2022-05-15 09:30:21 +0200

    U-Net implementations

--- Proj_284260_342454_342763/Miniproject_1/main.py
+++ Proj_284260_342454_342763/Miniproject_1/main.py
@@ -1,19 +1,28 @@
 import torch
 from model import *
 from others.utilities import *
-
+from torchvision import transforms
 import os
 import sys
-
+import timeit
 import matplotlib.pyplot as plt
+import numpy as np
+import random
 
 os.chdir(sys.path[0])
 
 
 def main():
+    start = timeit.default_timer()
+    torch.manual_seed(42)
+    torch.cuda.manual_seed(42)
+    np.random.seed(42)
+    random.seed(42)
+    torch.backends.cudnn.deterministic = True
     # Using a GPU if available
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print("Using {} as device".format(device))
+
 
     # Load the data    
     print('Loading the data...')
@@ -24,11 +33,20 @@ def main():
 
 
     # Select a subset to speed up computations
-    train_size = 1000
+    train_size = 50000
     train_input = train_input[:train_size]
     train_target = train_target[:train_size]
     test_input = test_input[:train_size]
     test_target = test_target[:train_size]
+
+    # Data augmentation
+    #trans = transforms.Compose([transforms.ColorJitter(brightness=.5,hue=.3),])
+    flip_transform = transforms.RandomHorizontalFlip(p=1)
+    #jitter = transforms.ColorJitter(brightness=.5,hue=.3)
+    train_input = torch.cat([train_input, flip_transform(train_input)],0)
+    train_target = torch.cat([train_target,flip_transform(train_target)],0)
+
+    # Data normalization
 
 
     # Convert the data into float type
@@ -43,52 +61,71 @@ def main():
     # Defining and training the model
     model = Model()
     model = model.to(device)
+    #model.load_pretrained_model()
+
     print('Training the model...')
-    model.train(train_input, train_target)
+    nb_epochs = 10
+    model.train(train_input, train_target, num_epochs=nb_epochs)
+
+    # Save the model
+    path_to_model = "./best_model.pth"
+    torch.save(model.state_dict(),path_to_model)
+
+
+    # Visual comparison between original images and reconstructions
+    # Just for us, probably we can't use matplotlib
+    # Try to see if there are visualization tools only in Pytorch
+
+    # Set "train" if you want to compare the training images, "test" for testing ones
+    images_to_visualize = "test"
+    #Set the number of images you want to see
+    n_comparisons = 30
+
+    if (images_to_visualize == "train"):
+        input = train_input
+        test = train_target
+    elif (images_to_visualize == "test"):
+        input = test_input
+        test = test_target
+    else:
+        raise RuntimeError("Che cazzo hai messo coglione")
+
+
+    for i in range(n_comparisons):
+        pred = model.predict(input[i].unsqueeze(0)).cpu()
+        f = plt.figure()
+        f.add_subplot(1, 3, 1)
+        fig1 = plt.imshow(input[i].cpu().int().permute(1, 2, 0))
+        fig1.axes.get_xaxis().set_visible(False)
+        fig1.axes.get_yaxis().set_visible(False)
+        f.add_subplot(1, 3, 2)
+        fig2 = plt.imshow(pred[0].int().permute(1, 2, 0))
+        fig2.axes.get_xaxis().set_visible(False)
+        fig2.axes.get_yaxis().set_visible(False)
+        f.add_subplot(1,3,3)
+        fig3 = plt.imshow(test[i].cpu().int().permute(1,2,0))
+        fig3.axes.get_xaxis().set_visible(False)
+        fig3.axes.get_yaxis().set_visible(False)
+        plt.show(block=True)
+
+
+    # Delete training samples
+    #del train_input,train_target
+    #if device.type =="cuda":
+        #torch.cuda.empty_cache()
+
 
     # Testing
     print('Using the trained model to denoise validation images...')
     with torch.no_grad():
-        prediction = model.predict(test_input)
+        prediction = model.predict(test_input.to(device))
 
     # Evaluating error
     error = psnr(prediction, test_target)
     print(f'The PSNR on the validation set is {error} DB')
 
-
-"""
-    # Visual comparison between original images and reconstructions
-    # Just for us, probably we can't use matplotlib
-    # Try to see if there are visualization tools only in Pytorch 
-
-    # Set "train" if you want to compare the training images, "test" for testing ones
-    images_to_visualize = "train"
-    if (images_to_visualize == "train"):
-        with torch.no_grad():
-            pred = model.predict(train_input).cpu()
-        train_input = train_input.cpu()
-        images = train_input
-    elif (images_to_visualize == "test"):
-        pred = prediction
-        test_input = test_input.cpu()
-        images = test_input
-        pred = pred.cpu()
-    else:
-        raise RuntimeError("Che cazzo hai messo coglione")
-
-    n_comparisons = 5
-    for i in range(n_comparisons):
-        f = plt.figure()
-        f.add_subplot(1, 2, 1)
-        fig1 = plt.imshow(images[i].int().permute(1, 2, 0))
-        fig1.axes.get_xaxis().set_visible(False)
-        fig1.axes.get_yaxis().set_visible(False)
-        f.add_subplot(1, 2, 2)
-        fig2 = plt.imshow(pred[i].int().permute(1, 2, 0))
-        fig2.axes.get_xaxis().set_visible(False)
-        fig2.axes.get_yaxis().set_visible(False)
-        plt.show(block=True)
-"""
+    stop = timeit.default_timer()
+    print('Time: ', stop - start)
 
 
 if __name__ == '__main__':
--- Proj_284260_342454_342763/Miniproject_1/others/utilities.py
+++ Proj_284260_342454_342763/Miniproject_1/others/utilities.py
@@ -1,6 +1,128 @@
 import torch
+from torch import nn
+import torch.nn.functional as F
+import torchvision
+from torch.nn.modules.utils import _pair, _quadruple
+from torchvision import models, transforms
+from torch.utils.data import Dataset, DataLoader
+
 
 def psnr(denoised, ground_truth):
     # Peak Signal to Noise Ratio : denoised and ground˙truth have range [0 , 1]
     mse = torch . mean ((denoised - ground_truth ) ** 2)
-    return -10 * torch . log10 ( mse + 10** -8)
+    return -10 * torch . log10(mse/(255**2) + 10**-8)
+
+
+class MedianPool2d(nn.Module):
+    """ Median pool (usable as median filter when stride=1) module.
+
+    Args:
+         kernel_size: size of pooling kernel, int or 2-tuple
+         stride: pool stride, int or 2-tuple
+         padding: pool padding, int or 4-tuple (l, r, t, b) as in pytorch F.pad
+         same: override padding and enforce same padding, boolean
+    """
+
+    def __init__(self, kernel_size=3, stride=1, padding=0, same=False):
+        super(MedianPool2d, self).__init__()
+        self.k = _pair(kernel_size)
+        self.stride = _pair(stride)
+        self.padding = _quadruple(padding)  # convert to l, r, t, b
+        self.same = same
+
+    def _padding(self, x):
+        if self.same:
+            ih, iw = x.size()[2:]
+            if ih % self.stride[0] == 0:
+                ph = max(self.k[0] - self.stride[0], 0)
+            else:
+                ph = max(self.k[0] - (ih % self.stride[0]), 0)
+            if iw % self.stride[1] == 0:
+                pw = max(self.k[1] - self.stride[1], 0)
+            else:
+                pw = max(self.k[1] - (iw % self.stride[1]), 0)
+            pl = pw // 2
+            pr = pw - pl
+            pt = ph // 2
+            pb = ph - pt
+            padding = (pl, pr, pt, pb)
+        else:
+            padding = self.padding
+        return padding
+
+    def forward(self, x):
+        # using existing pytorch functions and tensor ops so that we get autograd,
+        # would likely be more efficient to implement from scratch at C/Cuda level
+        x = F.pad(x, self._padding(x), mode='reflect')
+        x = x.unfold(2, self.k[0], self.stride[0]).unfold(3, self.k[1], self.stride[1])
+        x = x.contiguous().view(x.size()[:4] + (-1,)).median(dim=-1)[0]
+        return x
+
+
+
+def double_conv(in_c,out_c):
+    conv = nn.Sequential(
+        nn.Conv2d(in_c,out_c,kernel_size=3, padding=1),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(out_c, out_c, kernel_size=3, padding=1),
+        nn.ReLU(inplace=True)
+    )
+
+    return conv
+
+
+
+class ResNetBlock(nn.Module):
+    def __init__(self, nb_channels, kernel_size, batch_normalization=True, skip_connections=True):
+        super().__init__()
+
+        self.batch_normalization = batch_normalization
+        self.skip_connections = skip_connections
+
+        self.conv1 = nn.Conv2d(nb_channels, nb_channels,
+                               kernel_size = kernel_size,
+                               padding = (kernel_size - 1) // 2)
+
+        self.bn1 = nn.BatchNorm2d(nb_channels)
+
+        self.conv2 = nn.Conv2d(nb_channels, nb_channels,
+                               kernel_size = kernel_size,
+                               padding = (kernel_size - 1) // 2)
+
+        self.bn2 = nn.BatchNorm2d(nb_channels)
+
+    def forward(self, x):
+        y = self.conv1(x)
+        if self.batch_normalization : y = self.bn1(y)
+        y = F.relu(y)
+        y = self.conv2(y)
+        if self.batch_normalization: y = self.bn2(y)
+        if self.skip_connections: y = y + x
+        y = F.relu(y)
+
+        return y
+
+
+class MyDataset(Dataset):
+    def __init__(self, input, target, transform=None):
+        self.input = input
+        self.target = target
+        self.transform = transform
+
+    def __getitem__(self, index):
+        train_input = self.input[index]
+        train_target = self.target[index]
+        if self.transform is not None:
+            train_input = torch.cat([self.input, self.transform(self.input)],0)
+            train_target = torch.cat([self.target, self.transform(self.target)])
+
+        return train_input, train_target
+
+    def __len__(self):
+        if self.transform is not None:
+            return 3*self.input.size(0)
+
+        return self.input.size(0)
+
+
+
