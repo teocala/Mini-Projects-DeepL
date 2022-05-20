Update corrected implementation gradient

Proj_284260_342454_342763/Miniproject_2/Corrected implementation/model.py
@@ -36,16 +36,16 @@ class Model () :
     def __init__ ( self ) -> None :
         ## instantiate model + optimizer + loss function + any other stuff you need
         self.model = Sequential(# N, 3, 32, 32
-            # Conv2d(input_channels = 3, output_channels = 32, kernel_size = (5,5), stride = 2), # N, 32, 14, 14
-            # ReLU(),
-            # Conv2d(input_channels = 32, output_channels = 32, kernel_size = (3,3), stride = 2), # N, 32, 5, 5
-            # ReLU(),
-            # NearestUpsampling(scale_factor = 5, input_channels = 32, output_channels = 32, kernel_size = (5,5), stride = 2), #  N, 32, 15, 15
-            # ReLU(),
-            # NearestUpsampling(scale_factor = 5, input_channels = 32, output_channels = 3, kernel_size = (3,3), stride = 2), # N, 3, 32, 32
-            # Sigmoid()
-            Conv2d(input_channels = 3, output_channels = 32, kernel_size = (3,3), stride = 2), # N, 32, 15, 15
-            ReLU()
+            Conv2d(input_channels = 3, output_channels = 32, kernel_size = (5,5), stride = 2), # N, 32, 14, 14
+            ReLU(),
+            Conv2d(input_channels = 32, output_channels = 32, kernel_size = (3,3), stride = 2), # N, 32, 5, 5
+            ReLU(),
+            NearestUpsampling(scale_factor = 5, input_channels = 32, output_channels = 32, kernel_size = (5,5), stride = 2), #  N, 32, 15, 15
+            ReLU(),
+            NearestUpsampling(scale_factor = 5, input_channels = 32, output_channels = 3, kernel_size = (3,3), stride = 2), # N, 3, 32, 32
+            Sigmoid()
+            # Conv2d(input_channels = 3, output_channels = 32, kernel_size = (3,3), stride = 2), # N, 32, 15, 15
+            # ReLU()
         )
         self.criterion = MSE()
         self.optimizer = SGD(self.model.param(), lr=0.1)
@@ -60,13 +60,13 @@ class Model () :
         #: train˙target : tensor of size (N, C, H, W) containing another noisy version of the same images , which only differs from the input by their noise .
         set_grad_enabled(False)
         batch_size = 500
-        epochs = 3
+        epochs = 5
 
-        train_target_test = torch.arange(0,500*32*15*15).view(500,32,15,15) # Just to work with the right dimension with one Conv2D
+        # train_target_test = torch.randn((500,32,15,15)) # Just to work with the right dimension with one Conv2D
 
         for epoch in range(epochs):
             total_loss = 0
-            for batch_input, batch_target in zip(train_input.split(batch_size), train_target_test.split(batch_size)):
+            for batch_input, batch_target in zip(train_input.split(batch_size), train_target.split(batch_size)):
                 output = self.predict(batch_input)
                 loss = self.criterion.forward(output, batch_target)
                 total_loss += loss

Proj_284260_342454_342763/Miniproject_2/Corrected implementation/modules.py
@@ -105,18 +105,23 @@ class Conv2d(Module):
     def param(self):
         if not len(self.gradoutput) == 0: 
             unfolded = unfold(self.input, kernel_size=self.kernel_size, stride=self.stride)
-            self.dldw = (self.gradoutput.view(self.x_shape) @ unfolded.transpose(1,2)).sum(0)
+
+            unfolded.transpose_(1,2)
+            U = torch.zeros(unfolded.shape[0],self.output_channels,unfolded.shape[1],self.output_channels,unfolded.shape[2])
+
+            for idx in range(self.input_channels):
+                U[:,idx,:,idx,:] = unfolded
+
+
+            first_operand = self.gradoutput.view(self.x_shape) 
+            second_operand = U
+
+            self.dldw = torch.tensordot(first_operand, second_operand, dims = 3)
             self.dldw = self.dldw.view(self.weight.shape)
+
             
             self.dldb = (self.gradoutput.view(self.x_shape).sum(2)).sum(0) # again weight sharing
             self.dldb = self.dldb.view(self.bias.shape)
-
-            # self.dldw = self.gradoutput @ (self.input.T)
-            # self.dldb = self.gradoutput
-        
-        # list of zip to create list of tuples
-        # res1 = list(zip(self.weight, self.dldw))
-        # res2 = list(zip(self.bias, self.dldb))
 
         return [[self.weight, self.dldw],[self.bias, self.dldb]]
 
@@ -262,7 +267,7 @@ class SGD(Module):
                 a.copy_(p[0])
                 rhs.append(p[0] - self.lr*p[1])
                 # print(p[1])
-                print(f"p: {p[0]}", f"grad_p : {p[1]}")
+                # print(f"p: {p[0]}", f"grad_p : {p[1]}")
 
             module.update_params(rhs)
         # for p in self.parameters: