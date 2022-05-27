b3fc7c196a1c79dace5c33d486895eb0c0df35eb
Paolo Motta <paolo9.motta@mail.polimi.it>
2022-05-27 17:12:57 +0200

    Final version

--- Proj_284260_342454_342763/Miniproject_1/main.py
+++ Proj_284260_342454_342763/Miniproject_1/main.py
@@ -14,72 +14,45 @@ os.chdir(sys.path[0])
 
 def main():
     start = timeit.default_timer()
-    torch.manual_seed(42)
-    torch.cuda.manual_seed(42)
-    np.random.seed(42)
-    random.seed(42)
-    torch.backends.cudnn.deterministic = True
-    # Using a GPU if available
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print("Using {} as device".format(device))
 
+    '''
+    # To reproduce our results
+    seed = 42
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    '''
 
     # Load the data    
     print('Loading the data...')
     train_input, train_target = torch.load('train_data.pkl')
     test_input, test_target = torch.load('val_data.pkl')
-    train_input, train_target = train_input.to(device), train_target.to(device)
-    test_input, test_target = test_input.to(device), test_target.to(device)
 
-
-    # Select a subset to speed up computations
-    train_size = 50000
-    train_input = train_input[:train_size]
-    train_target = train_target[:train_size]
-    test_input = test_input[:train_size]
-    test_target = test_target[:train_size]
-
-    # Data augmentation
-    #trans = transforms.Compose([transforms.ColorJitter(brightness=.5,hue=.3),])
-    flip_transform = transforms.RandomHorizontalFlip(p=1)
-    #jitter = transforms.ColorJitter(brightness=.5,hue=.3)
-    train_input = torch.cat([train_input, flip_transform(train_input)],0)
-    train_target = torch.cat([train_target,flip_transform(train_target)],0)
-
-    # Data normalization
-
-
-    # Convert the data into float type
-    train_input = train_input.float()
-    train_target = train_target.float()
-    test_input = test_input.float()
-    test_target = test_target.float()
-
-    
     print(f'Training data of size {train_input.shape}')
 
     # Defining and training the model
     model = Model()
-    model = model.to(device)
-    #model.load_pretrained_model()
 
-    print('Training the model...')
     nb_epochs = 10
     model.train(train_input, train_target, num_epochs=nb_epochs)
 
-    # Save the model
-    path_to_model = "./best_model.pth"
-    torch.save(model.state_dict(),path_to_model)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    test_input = test_input.float()
+    test_target = test_target.float()
+    test_input = test_input.to(device)
+    test_target = test_target.to(device)
 
 
+    model.load_pretrained_model()
+    model.to(device)
+    model.unet.eval()
     # Visual comparison between original images and reconstructions
-    # Just for us, probably we can't use matplotlib
-    # Try to see if there are visualization tools only in Pytorch
-
     # Set "train" if you want to compare the training images, "test" for testing ones
     images_to_visualize = "test"
     #Set the number of images you want to see
     n_comparisons = 30
+    
 
     if (images_to_visualize == "train"):
         input = train_input
@@ -88,7 +61,7 @@ def main():
         input = test_input
         test = test_target
     else:
-        raise RuntimeError("Che cazzo hai messo coglione")
+        raise RuntimeError("Invalid split")
 
 
     for i in range(n_comparisons):
@@ -109,19 +82,13 @@ def main():
         plt.show(block=True)
 
 
-    # Delete training samples
-    #del train_input,train_target
-    #if device.type =="cuda":
-        #torch.cuda.empty_cache()
-
-
     # Testing
     print('Using the trained model to denoise validation images...')
     with torch.no_grad():
         prediction = model.predict(test_input.to(device))
 
     # Evaluating error
-    error = psnr(prediction, test_target)
+    error = compute_psnr(prediction, test_target)
     print(f'The PSNR on the validation set is {error} DB')
 
     stop = timeit.default_timer()
--- Proj_284260_342454_342763/Miniproject_1/others/utilities.py
+++ Proj_284260_342454_342763/Miniproject_1/others/utilities.py
@@ -1,75 +1,14 @@
 import torch
-from torch import nn
 import torch.nn.functional as F
-import torchvision
-from torch.nn.modules.utils import _pair, _quadruple
 from torchvision import models, transforms
 from torch.utils.data import Dataset, DataLoader
-
-
-def psnr(denoised, ground_truth):
-    # Peak Signal to Noise Ratio : denoised and ground˙truth have range [0 , 1]
-    mse = torch . mean ((denoised - ground_truth ) ** 2)
-    return -10 * torch . log10(mse/(255**2) + 10**-8)
-
-
-class MedianPool2d(nn.Module):
-    """ Median pool (usable as median filter when stride=1) module.
-
-    Args:
-         kernel_size: size of pooling kernel, int or 2-tuple
-         stride: pool stride, int or 2-tuple
-         padding: pool padding, int or 4-tuple (l, r, t, b) as in pytorch F.pad
-         same: override padding and enforce same padding, boolean
-    """
-
-    def __init__(self, kernel_size=3, stride=1, padding=0, same=False):
-        super(MedianPool2d, self).__init__()
-        self.k = _pair(kernel_size)
-        self.stride = _pair(stride)
-        self.padding = _quadruple(padding)  # convert to l, r, t, b
-        self.same = same
-
-    def _padding(self, x):
-        if self.same:
-            ih, iw = x.size()[2:]
-            if ih % self.stride[0] == 0:
-                ph = max(self.k[0] - self.stride[0], 0)
-            else:
-                ph = max(self.k[0] - (ih % self.stride[0]), 0)
-            if iw % self.stride[1] == 0:
-                pw = max(self.k[1] - self.stride[1], 0)
-            else:
-                pw = max(self.k[1] - (iw % self.stride[1]), 0)
-            pl = pw // 2
-            pr = pw - pl
-            pt = ph // 2
-            pb = ph - pt
-            padding = (pl, pr, pt, pb)
-        else:
-            padding = self.padding
-        return padding
-
-    def forward(self, x):
-        # using existing pytorch functions and tensor ops so that we get autograd,
-        # would likely be more efficient to implement from scratch at C/Cuda level
-        x = F.pad(x, self._padding(x), mode='reflect')
-        x = x.unfold(2, self.k[0], self.stride[0]).unfold(3, self.k[1], self.stride[1])
-        x = x.contiguous().view(x.size()[:4] + (-1,)).median(dim=-1)[0]
-        return x
+import torch.nn as nn
 
 
 
-def double_conv(in_c,out_c):
-    conv = nn.Sequential(
-        nn.Conv2d(in_c,out_c,kernel_size=3, padding=1),
-        nn.ReLU(inplace=True),
-        nn.Conv2d(out_c, out_c, kernel_size=3, padding=1),
-        nn.ReLU(inplace=True)
-    )
-
-    return conv
-
+def compute_psnr(x, y, max_range=255.0):
+    assert x.shape == y.shape and x.ndim == 4
+    return 20 * torch.log10(torch.tensor(max_range)) - 10 * torch.log10(((x-y) ** 2).mean((1,2,3))).mean()
 
 
 class ResNetBlock(nn.Module):
@@ -94,35 +33,258 @@ class ResNetBlock(nn.Module):
     def forward(self, x):
         y = self.conv1(x)
         if self.batch_normalization : y = self.bn1(y)
-        y = F.relu(y)
+        y = F.leaky_relu(y)
         y = self.conv2(y)
         if self.batch_normalization: y = self.bn2(y)
         if self.skip_connections: y = y + x
-        y = F.relu(y)
+        y = F.leaky_relu(y)
 
         return y
 
 
-class MyDataset(Dataset):
-    def __init__(self, input, target, transform=None):
-        self.input = input
-        self.target = target
-        self.transform = transform
+class ResNetBlock2(nn.Module):
+    def __init__(self, nb_channels, kernel_size, batch_normalization=True, skip_connections=True):
+        super().__init__()
+
+        self.batch_normalization = batch_normalization
+        self.skip_connections = skip_connections
+
+        self.conv1 = nn.Conv2d(nb_channels, nb_channels,
+                               kernel_size = kernel_size,
+                               padding = (kernel_size - 1) // 2)
+
+        self.bn1 = nn.GroupNorm(num_groups=3,num_channels=nb_channels)
+
+        self.conv2 = nn.Conv2d(nb_channels, nb_channels,
+                               kernel_size = kernel_size,
+                               padding = (kernel_size - 1) // 2)
+
+        self.bn2 = nn.GroupNorm(num_groups=3,num_channels=nb_channels)
+
+    def forward(self, x):
+        y = self.conv1(x)
+        if self.batch_normalization : y = self.bn1(y)
+        y = F.leaky_relu(y)
+        y = self.conv2(y)
+        if self.batch_normalization: y = self.bn2(y)
+        if self.skip_connections: y = y + x
+        y = F.leaky_relu(y)
+
+        return y
+
+
+
+class MyData(Dataset):
+    def __init__(self, train_input, train_target):
+        self.train_input = train_input
+        self.train_target = train_target
+        self.transform1 = transforms.RandomHorizontalFlip(p=1)
+        self.transform2 = transforms.RandomVerticalFlip(p=1)
 
     def __getitem__(self, index):
-        train_input = self.input[index]
-        train_target = self.target[index]
-        if self.transform is not None:
-            train_input = torch.cat([self.input, self.transform(self.input)],0)
-            train_target = torch.cat([self.target, self.transform(self.target)])
-
-        return train_input, train_target
+        return self.train_input[index], self.transform1(self.train_input[index]), self.transform2(self.train_input[index]), \
+               self.train_target[index], self.transform1(self.train_target[index]), self.transform2(self.train_target[index])
 
     def __len__(self):
-        if self.transform is not None:
-            return 3*self.input.size(0)
+        return self.train_input.size(0)
 
-        return self.input.size(0)
+
+def collate_batch(batch):
+    train_list = []
+    target_list = []
+    for item1,item2,item3, target1,target2,target3 in batch:
+        train_list.extend([item1,item2,item3])
+        target_list.extend([target1,target2,target3])
+
+    return torch.stack(train_list), torch.stack(target_list)
+
+
+class UNet(nn.Module):
+    """U-Net Architecture"""
+
+    def __init__(self, in_channels=3, out_channels=3):
+        """Initializes U-Net."""
+
+        super(UNet, self).__init__()
+
+        self._block1 = nn.Sequential(
+            nn.Conv2d(in_channels, 48, 3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(48, 48, 3, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(48,48,kernel_size=3,stride=2,padding=1))
+
+
+        self._block2 = nn.Sequential(
+            nn.Conv2d(48, 48, 3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(48, 48, 3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(48,48,kernel_size=3,padding=1,stride=2))
+
+
+        self._block3 = nn.Sequential(
+            nn.Conv2d(48, 48, 3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(48, 48, 3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.ConvTranspose2d(48, 48, 3, stride=2, padding=1, output_padding=1))
+
+
+        self._block4 = nn.Sequential(
+            nn.Conv2d(96, 96, 3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(96, 96, 3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.ConvTranspose2d(96, 96, 3, stride=2, padding=1, output_padding=1))
+
+
+        self._block5 = nn.Sequential(
+            nn.Conv2d(144, 96, 3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(96, 96, 3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.ConvTranspose2d(96, 96, 3, stride=2, padding=1, output_padding=1))
+
+
+        self._block6 = nn.Sequential(
+            nn.Conv2d(96 + in_channels, 64, 3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(64, 32, 3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(32, out_channels, 3, stride=1, padding=1),
+            )
+
+        # Initialize weights
+        self._init_weights()
+
+
+    def _init_weights(self):
+        """Initializes weights using Xavier initialization"""
+
+        for m in self.modules():
+            if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
+                nn.init.xavier_normal_(m.weight.data)
+                m.bias.data.zero_()
+
+
+    def forward(self, x):
+        """Through encoder, then decoder by adding U-skip connections. """
+
+        # Encoder
+        down1 = self._block1(x)
+        down2 = self._block2(down1)
+        down3 = self._block2(down2)
+        down4 = self._block2(down3)
+        down5 = self._block2(down4)
+
+        # Decoder
+        up5 = self._block3(down5)
+        concat5 = torch.cat((up5, down4), dim=1)
+        up4 = self._block4(concat5)
+        concat4 = torch.cat((up4, down3), dim=1)
+        up3 = self._block5(concat4)
+        concat3 = torch.cat((up3, down2), dim=1)
+        up2 = self._block5(concat3)
+        concat2 = torch.cat((up2, down1), dim=1)
+        up1 = self._block5(concat2)
+        concat1 = torch.cat((up1, x), dim=1)
+
+        # Final activation
+        return self._block6(concat1)
+
+
+
+class ResUNet(nn.Module):
+    """Residual U-Net Architecture"""
+
+    def __init__(self, in_channels=3, out_channels=3):
+        """Initializes Residual U-Net"""
+
+        super(ResUNet, self).__init__()
+
+        self.in_ = nn.Conv2d(in_channels,48,kernel_size=1,stride=1)
+
+        self._block1 = nn.Sequential(
+            ResNetBlock2(nb_channels=48,kernel_size=3,batch_normalization=True,skip_connections=True),
+            nn.MaxPool2d(2))
+
+        self._block2 = nn.Sequential(
+            ResNetBlock2(nb_channels=48,kernel_size=3,batch_normalization=True,skip_connections=True),
+            nn.ConvTranspose2d(48, 48, 3, stride=2, padding=1, output_padding=1))
+
+        self._block3 = nn.Sequential(
+            ResNetBlock2(nb_channels=96,kernel_size=3,batch_normalization=True,skip_connections=True),
+            nn.ConvTranspose2d(96, 96, 3, stride=2, padding=1, output_padding=1))
+
+
+        self._block4 = nn.Sequential(
+            nn.Conv2d(144, 96, 1, stride=1),
+            nn.LeakyReLU(0.1),
+            ResNetBlock2(nb_channels=96,kernel_size=3,batch_normalization=True,skip_connections=True),
+            nn.ConvTranspose2d(96, 96, 3, stride=2, padding=1, output_padding=1))
+
+
+        self._block5 = nn.Sequential(
+            nn.Conv2d(96 + in_channels, 64, 3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(64, 32, 3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(32, out_channels, 3, stride=1, padding=1),
+            )
+
+        # Initialize weights
+        self._init_weights()
+
+
+    def _init_weights(self):
+        """Initializes weights using Xavier initizialitation"""
+
+        for m in self.modules():
+            if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
+                nn.init.xavier_normal_(m.weight.data)
+                m.bias.data.zero_()
+
+
+    def forward(self, x):
+        """Through encoder, then decoder by adding U-skip connections. """
+
+        # Encoder
+        x_in = self.in_(x)
+        down1 = self._block1(x_in)
+        down2 = self._block1(down1)
+        down3 = self._block1(down2)
+        down4 = self._block1(down3)
+        down5 = self._block1(down4)
+
+        # Decoder
+        up5 = self._block2(down5)
+        concat5 = torch.cat((up5, down4), dim=1)
+        up4 = self._block3(concat5)
+        concat4 = torch.cat((up4, down3), dim=1)
+        up3 = self._block4(concat4)
+        concat3 = torch.cat((up3, down2), dim=1)
+        up2 = self._block4(concat3)
+        concat2 = torch.cat((up2, down1), dim=1)
+        up1 = self._block4(concat2)
+        concat1 = torch.cat((up1, x), dim=1)
+
+        # Final activation
+        return self._block5(concat1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
